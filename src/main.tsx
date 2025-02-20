888ec4da49fa332f8e9070dff0e9800ca84e4bad
nhson <nhson.codes@gmail.com>
2025-02-20 23:22:24 +0700

    ✨ Config ReactQueryDevtools

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,19 @@
 import { createRoot } from 'react-dom/client'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import './i18n'
-import App from './App'
 import { GlobalStyles } from 'components'
+import { lazy } from 'react'
+
+const queryClient = new QueryClient()
+const LazyApp = lazy(() => import('./App'))
 
 createRoot(document.getElementById('root')!).render(
-  <GlobalStyles>
-    <App />
-  </GlobalStyles>,
+  <QueryClientProvider client={queryClient}>
+    <GlobalStyles>
+      <LazyApp />
+    </GlobalStyles>
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>,
 )
